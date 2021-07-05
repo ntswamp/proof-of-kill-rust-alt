d6dfa566fb5830d1959e516ab8c299ca63a91753
hanbyo <han@platinum-egg.com>
2021-07-05 19:10:40 +0900

    wip

--- src/fight.rs
+++ src/fight.rs
@@ -9,32 +9,55 @@
 use crate::character::Character;
 use rand::Rng;
 
-pub fn versus(fighter1: &mut impl Character,fighter2:&mut impl Character) -> FightResult{
-    let mut random_seed : Vec<i32> = vec![];
+pub fn versus(fighter1: &mut impl Character,fighter2:&mut impl Character, random_seed:Option<Vec<i32>>) -> FightResult{
+    match random_seed {
+        None => {
+            let mut random_seed : Vec<i32> = vec![];
 
-    let mut rng = rand::thread_rng();
-    while fighter1.current_health() > 0 && fighter2.current_health() > 0 {
-        //-5 ~ 5 inclusively
-        let randomness:i32 = rng.gen_range(-5..=5);
-        random_seed.push(randomness);
-        //decide the first-mover
-        if fighter1.current_action() > fighter2.current_action() {
-            fighter2.take_damage(fighter1.produce_damage(randomness));
-            fighter1.take_damage(fighter2.produce_damage(randomness));
-        } else {
-            fighter1.take_damage(fighter2.produce_damage(randomness));
-            fighter2.take_damage(fighter1.produce_damage(randomness));
-        }
-        fighter1.regenerate();
-        fighter2.regenerate();
-        
-        fighter1.report_point();
-        fighter2.report_point();
+            let mut rng = rand::thread_rng();
+            while fighter1.current_health() > 0 && fighter2.current_health() > 0 {
+                //-5 ~ 5 inclusively
+                let randomness:i32 = rng.gen_range(-5..=5);
+                random_seed.push(randomness);
+                //decide the first-mover
+                if fighter1.current_action() > fighter2.current_action() {
+                    fighter2.take_damage(fighter1.produce_damage(randomness));
+                    fighter1.take_damage(fighter2.produce_damage(randomness));
+                } else {
+                    fighter1.take_damage(fighter2.produce_damage(randomness));
+                    fighter2.take_damage(fighter1.produce_damage(randomness));
+                }
+                fighter1.regenerate();
+                fighter2.regenerate();
+
+                fighter1.report_point();
+                fighter2.report_point();
+            }
+            return FightResult::make(random_seed);
+        },
+        Some(random_seed) => {
+            for randomness in &random_seed{
+                if fighter1.current_action() > fighter2.current_action() {
+                    fighter2.take_damage(fighter1.produce_damage(*randomness));
+                    fighter1.take_damage(fighter2.produce_damage(*randomness));
+            } else {
+                    fighter1.take_damage(fighter2.produce_damage(*randomness));
+                    fighter2.take_damage(fighter1.produce_damage(*randomness));
+            }
+            fighter1.regenerate();
+            fighter2.regenerate();
+            }
+            return FightResult::make(random_seed);
+        },
     }
-    FightResult::make(random_seed)
-    
 }
 
+/**
+ * 
+ * FightResult ought to store a hash which is converted from maybe the entire serialized data of the fight to verify.
+ * by doing that, "VerifyFight()" can compare two hashes to check if the local version of the fight's hash differs from the verified one.
+ * 
+ **/
 
 pub struct FightResult {
     winner: String,
@@ -50,8 +73,7 @@ impl FightResult {
     }
 }
 
-fn VerifyFight(random_seed: Vec<i32>,fighter1:&mut impl Character,fighter2:&mut impl Character) -> FightResult{
-    FightResult {
-        winner:String::from("unimplemented"),
-    }
+fn VerifyFight(random_seed: Vec<i32>,fighter1:&mut impl Character,fighter2:&mut impl Character, original_result:&FightResult) -> bool{
+    //TODO
+    true
 }

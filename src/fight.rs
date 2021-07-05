35c03fdd7490d36689403166fb53c88ad0ed4243
hanbyo <han@platinum-egg.com>
2021-07-05 14:29:45 +0900

    WIP

--- src/fight.rs
+++ src/fight.rs
@@ -1,8 +1,13 @@
 use crate::character::Character;
+use rand::Rng;
 
 pub fn versus(fighter1: &mut impl Character,fighter2:&mut impl Character) -> FightResult{
+    let mut random_seed : Vec<i32> = vec![];
 
+    let mut rng = rand::thread_rng();
     while fighter1.current_health() > 0 && fighter2.current_health() > 0 {
+        let randomness:i32 = rng.gen_range(-5..=5);
+        random_seed.push(randomness);
         //decide the first-mover
         if fighter1.current_action() > fighter2.current_action() {
             fighter2.take_damage(fighter1.produce_damage());
@@ -17,7 +22,7 @@ pub fn versus(fighter1: &mut impl Character,fighter2:&mut impl Character) -> Fig
         fighter1.report_point();
         fighter2.report_point();
     }
-    FightResult::make()
+    FightResult::make(random_seed)
     
 }
 
@@ -27,13 +32,17 @@ pub struct FightResult {
 }
 
 impl FightResult {
-    pub fn make() -> FightResult {
+    pub fn make(random_seed:Vec<i32>) -> FightResult {
+        println!("random sequence:{:#?}",random_seed);
         FightResult {
-            winner: String::from("ntswamp"),
+            winner:String::from("unimplemented"),
         }
+
     }
 }
 
-fn VerifyFight() {
-
+fn VerifyFight(random_seed: Vec<i32>,fighter1:&mut impl Character,fighter2:&mut impl Character) -> FightResult{
+    FightResult {
+        winner:String::from("unimplemented"),
+    }
 }
